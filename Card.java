ac425855b06eb566c1b6b9bb4378dccb307e94cc
matheusecke <111882406+matheusecke@users.noreply.github.com>
2023-09-27 13:16:27 -0300

    Update Card.java

--- Card.java
+++ Card.java
@@ -13,6 +13,55 @@ public class Card
    // return String representation of Card
    public String toString() 
    { 
-      return face + " of " + suit;
+      return face + " de " + suit;
    } 
+
+   // sets card value
+   /*public int handValue(Card card)
+   {
+      String view_face = card.getFace();
+      switch(view_face)
+      {
+            case "As":
+               if(hand_value < 11)
+                  hand_value = hand_value + 11;
+               else
+                  hand_value = hand_value + 1;
+               break;
+            case "Dois":
+               hand_value = hand_value + 2;
+               break;
+            case "Tres":
+               hand_value = hand_value + 3;
+               break;
+            case "Quatro":
+               hand_value = hand_value + 4;
+               break;
+            case "Cinco":
+               hand_value = hand_value + 5;
+               break;
+            case "Seis":
+               hand_value = hand_value + 6;
+               break;
+            case "Sete":
+               hand_value = hand_value + 7;
+               break;
+            case "Oito":
+               hand_value = hand_value + 8;
+               break;
+            case "Nove":
+               hand_value = hand_value + 9;
+               break;
+            default:
+               hand_value = hand_value + 10;
+      }
+
+      return hand_value;
+   }*/
+
+   public String getFace()
+   {
+      return face;
+   }
+   
 } // end class Card
