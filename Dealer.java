b6dbb05d544b858d9f429e38b326dce281f81efb
matheusecke <111882406+matheusecke@users.noreply.github.com>
2023-09-27 13:18:04 -0300

    Create Dealer.java

---
+++ Dealer.java
@@ -0,0 +1,78 @@
+public class Dealer
+{
+    private Card card;
+    private Card[] hand = new Card[10];
+    int hand_value = 0;
+
+    public void setCard(Card card, int hand_pos)
+    {
+        this.hand[hand_pos] = card;
+    }
+
+    public int handValue(Card card)
+    {
+        String view_face = card.getFace();
+        switch(view_face)
+        {
+            case "As":
+                if(hand_value < 11)
+                    hand_value = hand_value + 11;
+                else
+                    hand_value = hand_value + 1;
+                break;
+            case "Dois":
+                hand_value = hand_value + 2;
+                break;
+            case "Tres":
+                hand_value = hand_value + 3;
+                break;
+            case "Quatro":
+                hand_value = hand_value + 4;
+                break;
+            case "Cinco":
+                hand_value = hand_value + 5;
+                break;
+            case "Seis":
+                hand_value = hand_value + 6;
+                break;
+            case "Sete":
+                hand_value = hand_value + 7;
+                break;
+            case "Oito":
+                hand_value = hand_value + 8;
+                break;
+            case "Nove":
+                hand_value = hand_value + 9;
+                break;
+            default:
+                hand_value = hand_value + 10;
+        }
+
+        return hand_value;
+    }
+
+    public void printID()
+    {
+        System.out.printf("\nDEALER\n----------\n");
+    }
+
+    public void printCardsStart()
+    {
+        for (int i = 0; i < 2; i++) 
+        {
+            if(this.hand[i] != null && i != 1)
+                System.out.printf("%s\n",this.hand[i]);
+            else if(this.hand[i] != null && i == 1)
+                System.out.printf("~CARTA VIRADA~\n");
+        }
+    }
+
+    public void printCardsEnd()
+    {
+        for (int i = 0; i < 2; i++) 
+        {
+            if(this.hand[i] != null)
+            System.out.printf("%s\n",this.hand[i]);   
+        }
+    }
+}
