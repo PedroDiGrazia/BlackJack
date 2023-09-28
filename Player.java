063981864c1d03228f2610626a8b8a0b4ccae5dd
matheusecke <111882406+matheusecke@users.noreply.github.com>
2023-09-28 19:14:52 -0300

    Update Player.java

--- Player.java
+++ Player.java
@@ -64,10 +64,16 @@ public class Player
 
     public void printCards()
     {
-        for (int i = 0; i < 2; i++) 
+        int i = 0;
+        while(this.hand[i] != null)
         {
-            if(this.hand[i] != null)
-            System.out.printf("%s\n",this.hand[i]);   
+            System.out.printf("%s\n",this.hand[i]);
+            i++; 
         }
+    }
+
+    public Card[] getHand()
+    {
+        return hand;
     }
 }
